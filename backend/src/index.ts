dd83690163f461265a3fdf0cab18b89c78470870
Abhijith V <abhijithababhijith@gmail.com>
2025-06-21 22:48:17 +0530

    better auth backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import 'dotenv/config'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.use(express.json());
+
+app.get("/health-check", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Server is running"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port ", PORT);
+});
